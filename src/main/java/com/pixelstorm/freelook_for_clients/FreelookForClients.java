67e7f943585cfc6b1e06d8c5b0e200e9aa1f08a3
Pixelstorm <thepixelstormer@gmail.com>
2023-06-22 18:49:46 +0100

    Remove unused code

--- src/main/java/com/pixelstorm/freelook_for_clients/FreelookForClients.java
+++ src/main/java/com/pixelstorm/freelook_for_clients/FreelookForClients.java
@@ -3,19 +3,12 @@ package com.pixelstorm.freelook_for_clients;
 import org.lwjgl.glfw.GLFW;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBind;
 import net.minecraft.client.option.StickyKeyBind;
 
 public class FreelookForClients implements ClientModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod name as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Freelook For Clients");
-
 	public static final KeyBind HOLD_FREE_LOOK_KEYBIND = KeyBindingHelper.registerKeyBinding(
 			new KeyBind("key.freelook_for_clients.hold", GLFW.GLFW_KEY_WORLD_1,
 					"key.categories.freelook_for_clients"));
@@ -26,9 +19,5 @@ public class FreelookForClients implements ClientModInitializer {
 
 	@Override
 	public void onInitializeClient(ModContainer mod) {
-		// ClientTickEvents.START.register(client -> {
-		// LOGGER.info("{}, {}", HOLD_FREE_LOOK_KEYBIND.getKeyName(),
-		// HOLD_FREE_LOOK_KEYBIND.getKeyTranslationKey());
-		// });
 	}
 }
